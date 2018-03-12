68caa07ea0af3d04e9d26a5cd9e86be3f645a3c8
Nikron <nikron95@gmail.com>
2018-03-12 22:31:26 +0100

    v0.1.1    Simple rss reader in asp.net mvc

--- RssReader/RssReader/Controllers/RssController.cs
+++ RssReader/RssReader/Controllers/RssController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Xml.Linq;
 using RssReader.Models;
@@ -11,18 +9,18 @@ namespace RssReader.Controllers
     public class RssController : Controller
     {
         // GET: Rss
+        private readonly IEnumerable<XElement> _rss =
+            XElement.Load("https://news.google.com/news/rss/?ned=pl_pl&gl=PL&hl=pl").Descendants("item");
+
         public ActionResult Index()
         {
             var RMList = new List<RssModel>();
-            var x = XElement.Load("https://news.google.com/news/rss/?ned=pl_pl&gl=PL&hl=pl").Descendants("item");
-            foreach (var item in x)
-            {
+            foreach (var item in _rss)
                 RMList.Add(new RssModel(
                     item.Element("title").Value,
                     item.Element("category").Value,
                     DateTime.Parse(item.Element("pubDate").Value),
                     item.Element("description").Value));
-            }
             return View(RMList);
         }
     }
