4804e88abdaecd4ce50f2e9e629e56f53f8500ae
Nikron <nikron95@gmail.com>
2018-03-13 15:46:25 +0100

    Added aps net core version. Minifing and bundling.

---
+++ RssReader/AspNetCore/Controllers/RssController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using AspNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Controllers
+{
+    public class RssController : Controller
+    {
+        // GET: Rss
+        private readonly IEnumerable<XElement> _rss =
+            XElement.Load("https://news.google.com/news/rss/?ned=pl_pl&gl=PL&hl=pl").Descendants("item");
+
+        public ActionResult Index()
+        {
+            var RMList = new List<RssModel>();
+            foreach (var item in _rss)
+                RMList.Add(new RssModel(
+                    item.Element("title").Value,
+                    item.Element("category").Value,
+                    DateTime.Parse(item.Element("pubDate").Value),
+                    item.Element("description").Value));
+            return View(RMList);
+        }
+    }
+}
---
+++ RssReader/AspNetCore/Models/RssModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AspNetCore.Models
+{
+    public class RssModel
+    {
+        public RssModel(string title, string category, DateTime date, string description)
+        {
+            Title = title;
+            Category = category;
+            Date = date;
+            Description = description;
+        }
+
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
