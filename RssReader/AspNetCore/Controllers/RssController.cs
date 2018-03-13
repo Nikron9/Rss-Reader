a97a31f4c705c734ca170d71727efe385041d17f
Nikron <nikron95@gmail.com>
2018-03-14 00:41:39 +0100

    v0.2.1   asp net core styled and added pagination and page items number select

--- RssReader/AspNetCore/Controllers/RssController.cs
+++ RssReader/AspNetCore/Controllers/RssController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Linq;
 using AspNetCore.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -12,16 +13,25 @@ namespace AspNetCore.Controllers
         private readonly IEnumerable<XElement> _rss =
             XElement.Load("https://news.google.com/news/rss/?ned=pl_pl&gl=PL&hl=pl").Descendants("item");
 
-        public ActionResult Index()
+        public List<RssModel> RMList = new List<RssModel>();
+
+        public string TtleToSearch { get; set; }
+
+        public IActionResult Index(int? page, int? pageSize, string titleToSearch)
         {
-            var RMList = new List<RssModel>();
+            TtleToSearch = titleToSearch;
+            ViewBag.titleToSearch = titleToSearch;
+            ViewBag.pageSize = pageSize;
             foreach (var item in _rss)
                 RMList.Add(new RssModel(
                     item.Element("title").Value,
                     item.Element("category").Value,
                     DateTime.Parse(item.Element("pubDate").Value),
                     item.Element("description").Value));
-            return View(RMList);
+            if (!string.IsNullOrEmpty(titleToSearch))
+                RMList = RMList.Where(x => x.Title.Contains(titleToSearch)).ToList();
+
+            return View(Pagination<RssModel>.Create(RMList.AsQueryable(), page ?? 1, pageSize ?? 4));
         }
     }
 }
---
+++ RssReader/AspNetCore/Models/Pagination.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AspNetCore.Models
+{
+    public class Pagination<T> : List<T>
+    {
+        public Pagination(IEnumerable<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int) Math.Ceiling(count / (double) pageSize);
+
+            AddRange(items);
+        }
+
+        public int PageIndex { get; }
+        public int TotalPages { get; }
+
+        public bool HasPreviousPage => PageIndex > 1;
+
+        public bool HasNextPage => PageIndex < TotalPages;
+
+        public static Pagination<T> Create(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+            return new Pagination<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
